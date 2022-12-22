57876d3f725934a907bf9deb2cbbfb5ef2c59c64
Pierson <pstrimar@gmail.com>
2022-12-22 13:50:48 -0500

    added ripple effect

--- Assets/Scripts/Clicky.cs
+++ Assets/Scripts/Clicky.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 
 public class Clicky : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer[] renderers;
+    [SerializeField] MeshRenderer[] renderers;
+    [SerializeField] GameObject scanPrefab;
+    [SerializeField] float scanScaleMultiplier = 2f;
     
     private static List<Material> materials = new List<Material>();
     private static bool _isActive = false;
     private static float _lerpPos = .85f;
     private static float illuminationFactor;
+    //private static GameObject scanVFX;
+    private static List<GameObject> scanVFX = new List<GameObject>();
+    private static float scanStartScale1 = 0.08f;
+    private static float scanStartScale2 = 0.08f;
+    private static float scanStartScale3 = 0.08f;
+    private static float[] scanStartScale = new float[] {.08f, 1.08f, 2.08f};
     
     void Start()
     {
@@ -18,6 +26,13 @@ public class Clicky : MonoBehaviour
             materials.Add(r.material);
             r.material.SetFloat("_Atten", 1.25f);
         }
+        for (int i = 0; i < 3; i++)
+        {
+            var scanObject = Instantiate(scanPrefab, Vector3.zero, Quaternion.identity);
+            scanObject.SetActive(false);
+            scanObject.transform.localScale = new Vector3(scanStartScale[i], scanStartScale[i], scanStartScale[i]);
+            scanVFX.Add(scanObject);
+        }
     }
 
     void Update()
@@ -25,9 +40,21 @@ public class Clicky : MonoBehaviour
         //The effect is active so change attenuation over time
         if (_isActive)
         {
+            foreach (var scanObj in scanVFX)
+            {
+                if (scanObj.transform.localScale.x < 10f)
+                {
+                    scanObj.transform.localScale += new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime) * scanScaleMultiplier;
+                }
+                else
+                {
+                    scanObj.SetActive(false);                
+                }
+            }
+
             if (_lerpPos < 1.25f)
             {
-                _lerpPos += Time.deltaTime / 10;
+                _lerpPos += Time.deltaTime / 5;
                 foreach (Material m in materials)
                 {
                     m.SetFloat("_Atten", _lerpPos * illuminationFactor);
@@ -37,12 +64,18 @@ public class Clicky : MonoBehaviour
             {
                 _isActive = false;
             }
-        }
+        }        
     }
 
     public static void Illuminate(Vector3 hitPos, Vector3 hitNormal, float multiplier = 1f)
     {
         illuminationFactor = multiplier;
+        for (int i = 0; i < 3; i++)
+        {
+            scanVFX[i].transform.position = hitPos;
+            scanVFX[i].transform.localScale = new Vector3(scanStartScale[i], scanStartScale[i], scanStartScale[i]);
+            scanVFX[i].SetActive(true);
+        }
         hitPos += hitNormal * 1f;
         foreach (Material m in materials)
         {
